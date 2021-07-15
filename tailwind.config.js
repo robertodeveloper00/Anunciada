b5bf5ba3ac936c4bdec66265139f3d9c233a5b71
Roberto Barrientos <robertodeveloper00u@gmail.comgit config --global user.name>
2021-07-15 13:33:50 -0600

    Anunciada Projected developed with Tailwind. First Commmit

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'primary-light': '#FAEED7',
+        primary: '#FCDA99',
+        terciary: '#A64141',
+        'terciary-dark': '#772b2b',
+        'complem-gray': '#648381',
+        secondary: '#183642',
+      },
+    },
+
+    fontFamily: {
+      display: ['Merriweather', 'serif'],
+      paragraph: ['Open Sans', 'sans-serif'],
+    },
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        lg: '1124px',
+        xl: '1124px',
+        '2xl': '1124px',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
